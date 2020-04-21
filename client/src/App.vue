f0fa9346ed78dc2c3c1dadee74dc8c0380d035ff
Maksim Burchak <frazo4ka@gmail.com>
2020-04-21 23:41:18 +0300

    init: setup vue project instead of pure js

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+import './app.scss';
+
+export default {
+  name: 'App'
+}
+</script>
